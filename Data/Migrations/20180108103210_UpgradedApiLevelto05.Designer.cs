772eb9de185f4361fac139bb8267fa623e3c9974
Anduin <Anduin@aiursoft.com>
2018-01-08 18:34:35 +0800

    create database update script.

--- Data/Migrations/20180108103210_UpgradedApiLevelto05.Designer.cs
+++ Data/Migrations/20180108103210_UpgradedApiLevelto05.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Aiursoft.Developer.Data.Migrations
 {
     [DbContext(typeof(DeveloperDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180108103210_UpgradedApiLevelto05")]
+    partial class UpgradedApiLevelto05
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
