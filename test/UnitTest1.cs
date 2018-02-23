963be93b4374496a1c44d0c725309bccf9e24e98
Anduin Xue <anduin@aiursoft.com>
2018-02-23 10:54:35 +0800

    Created test project.

---
+++ test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Aiursoft.Developer.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
